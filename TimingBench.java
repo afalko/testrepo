56484e03e2aeafab7ea846a12e4f03b81088b0d0
Andrey Falko <afalko@salesforce.com>
2018-01-31 12:24:28 -0800

    Extra underscore handling

--- TimingBench.java
+++ TimingBench.java
@@ -16,6 +16,24 @@ public class TimingBench {
         return resultBuilder.toString();
     }
 
+    static String toSnakeAndLowerCase2(String attrName) {
+        if (attrName == null || attrName.isEmpty()) {
+          return attrName;
+        }
+        char firstChar = attrName.subSequence(0, 1).charAt(0);
+        boolean prevCharIsUpperCaseOrUnderscore = Character.isUpperCase(firstChar) || firstChar == '_';
+        StringBuilder resultBuilder = new StringBuilder().append(Character.toLowerCase(firstChar));
+        for (char attrChar : attrName.substring(1).toCharArray()) {
+          boolean charIsUpperCase = Character.isUpperCase(attrChar);
+          if (!prevCharIsUpperCaseOrUnderscore && charIsUpperCase) {
+            resultBuilder.append("_");
+          }
+          resultBuilder.append(Character.toLowerCase(attrChar));
+          prevCharIsUpperCaseOrUnderscore = charIsUpperCase || attrChar == '_';
+        }
+        return resultBuilder.toString();
+      }
+
     public static void main(String[] args) {
         final Pattern snakeCasePattern = Pattern.compile("([a-z0-9])([A-Z])");
         String attrName = "StartTime_$1_$2";
@@ -33,6 +51,14 @@ public class TimingBench {
         startTime = System.currentTimeMillis();
         for (int i = 0; i < 1000000; i++) {
 			result = toSnakeAndLowerCase(attrName);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println(result);
+        System.out.println(endTime - startTime);
+
+		startTime = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            result = toSnakeAndLowerCase2(attrName);
         }
         endTime = System.currentTimeMillis();
         System.out.println(result);
